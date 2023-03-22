087e4e519048bbe0f252551be0c6bdec6dba4c95
putdee101 <128128725+putdee101@users.noreply.github.com>
2023-03-22 10:59:40 -0400

    Add files via upload

---
+++ footer.config-0627f07c.js
@@ -0,0 +1,20 @@
+var config = {
+  fallbackDisableSubmit: false,
+  version: '1.0.0',
+  controlName: 'Footer',
+  description: 'Custom Footer',
+  groupName: 'Header/Footer',
+  standardProperties: {
+    readOnly: true,
+    required: true,
+    description: false
+  },
+  properties: {
+    titleField: {
+      type: 'string',
+      title: 'Title'
+    }
+  }
+};
+
+export { config };
