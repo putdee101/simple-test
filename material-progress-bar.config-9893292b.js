e3eeb7eec5aa4b174139c753fd15ba178e649117
putdee101 <128128725+putdee101@users.noreply.github.com>
2023-03-28 12:23:34 -0400

    Add files via upload

---
+++ material-progress-bar.config-9893292b.js
@@ -0,0 +1,29 @@
+var config = {
+  fallbackDisableSubmit: false,
+  version: '1.0.0',
+  controlName: 'Progress',
+  description: 'Material Progress',
+  groupName: 'Material',
+  standardProperties: {
+    readOnly: true,
+    required: true,
+    description: false
+  },
+  properties: {
+    titleField: {
+      type: 'string',
+      title: 'Title'
+    },
+    disabled: {
+      type: 'boolean',
+      title: 'Disabled'
+    },
+    value: {
+      type: 'number',
+      isValueField: true,
+      title: 'Values'
+    }
+  }
+};
+
+export { config };
