29616b5212382bf708eea1224dc198a90d1c209a
putdee101 <128128725+putdee101@users.noreply.github.com>
2023-04-17 11:15:30 -0400

    Add files via upload

---
+++ footer.config-2dbbab35.js
@@ -0,0 +1,15 @@
+var config = {
+  fallbackDisableSubmit: false,
+  version: '1.0.0',
+  controlName: 'Footer',
+  description: 'Custom Footer',
+  groupName: 'Lilly Plugins',
+  standardProperties: {
+    readOnly: true,
+    required: true,
+    description: false
+  },
+  properties: {}
+};
+
+export { config };
