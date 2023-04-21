2ab58c9a19688286cfa751d24acf882178b8d351
putdee101 <128128725+putdee101@users.noreply.github.com>
2023-04-21 18:49:35 -0400

    Update smarty-address-control1.js

--- smarty-address-control1.js
+++ smarty-address-control1.js
@@ -96,7 +96,10 @@ export default class AddressAutoComplete extends LitElement {
             composed: true,
             detail: addressObject,
         };
-        console.log(addressObject);
+        document.getElementById('_03f229cc473dbbe93aad9adf9d9b956a').value = addressObject.street_line;
+        document.getElementById('_40c88161d76163ec7d870d48e2b3e35e').value = addressObject.city;
+        document.getElementById('_1507f946234885ea870b5cc8c0c4f0ae').value = addressObject.state;
+        document.getElementById('_b4d114d7eeb0b7cd17863bac2b068ec6').value = addressObject.zipcode;
         const nintexEvent = new CustomEvent('ntx-value-change', args);
         this.dispatchEvent(nintexEvent);
     }
