073cf5fbc2effc709ea9c229033f5fcaf69e999f
putdee101 <128128725+putdee101@users.noreply.github.com>
2023-04-25 12:16:26 -0400

    Update smarty-address-control5.js

--- smarty-address-control5.js
+++ smarty-address-control5.js
@@ -64,7 +64,7 @@ export default class AddressAutoComplete extends LitElement {
         return  html`
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-            <label>STREET ADDRESS</label>
+            <label class="nx-form-label nx-break-word nx-theme-label-1"><span class="nx-title">STREET ADDRESS</span></label>
             <div class="input-group input-group-sm" id="input-container" style="line-height: 1rem;">            
                 <input type="text" class="form-control" value="${this.inputValue}" @keyup=${this.updateInputValue} aria-label="address" id="address" style="font-size: 14px; padding: 7px 12px 7px 12px; ${inputStyle}"/>
             </div>
