8eaba38f1c9c8f89209aab0ae42e3f29bf6fe6f1
putdee101 <128128725+putdee101@users.noreply.github.com>
2023-04-27 20:39:31 -0400

    Add files via upload

---
+++ footer.config-6a642eae.js
@@ -0,0 +1,60 @@
+var config = {
+  fallbackDisableSubmit: false,
+  version: '1.0.0',
+  controlName: 'Footer',
+  description: 'Custom Footer',
+  groupName: 'Lilly Plugins',
+  standardProperties: {
+    readOnly: true,
+    required: true,
+    description: false
+  },
+  properties: {
+    link1Text: {
+      title: 'Link1 Text',
+      type: 'string'
+    },
+    link1URL: {
+      title: 'Link1 URL',
+      type: 'string'
+    },
+    link2Text: {
+      title: 'Link2 Text',
+      type: 'string'
+    },
+    link2URL: {
+      title: 'Link2 URL',
+      type: 'string'
+    },
+    link3Text: {
+      title: 'Link3 Text',
+      type: 'string'
+    },
+    link3URL: {
+      title: 'Link3 URL',
+      type: 'string'
+    },
+    firstLine: {
+      title: 'First Line Text',
+      type: 'string'
+    },
+    secondLine: {
+      title: 'Second Line Text',
+      type: 'string'
+    },
+    thirdLine: {
+      title: 'Third Line Text',
+      type: 'string'
+    },
+    thirdLinePhoneNumber: {
+      title: 'Third Line Phone Number',
+      type: 'string'
+    },
+    singleLine: {
+      title: 'Single Line Text',
+      type: 'string'
+    }
+  }
+};
+
+export { config };
