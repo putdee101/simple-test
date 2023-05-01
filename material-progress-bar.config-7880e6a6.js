934e8a86667c87d3054652965aba453419cbebde
putdee101 <128128725+putdee101@users.noreply.github.com>
2023-04-30 23:39:59 -0400

    Add files via upload

---
+++ material-progress-bar.config-7880e6a6.js
@@ -0,0 +1,25 @@
+var config = {
+  fallbackDisableSubmit: false,
+  version: '1.0.0',
+  controlName: 'Progress',
+  description: 'Material Progress',
+  groupName: 'Lilly Plugins',
+  standardProperties: {
+    readOnly: true,
+    required: true,
+    description: false,
+    visibility: true
+  },
+  properties: {
+    currentPage: {
+      type: 'number',
+      title: 'CurrentPage'
+    },
+    totalPages: {
+      type: 'number',
+      title: 'TotalPages'
+    }
+  }
+};
+
+export { config };
